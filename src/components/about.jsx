357a4aa6ab502dc8a4a9b2fa2e605240c931ee45
aljohn <johnnjill3@gmail.com>
2019-07-25 23:42:41 +0800

    adding another paragraph

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,9 +5,14 @@ const About = () => {
     <React.Fragment>
       <h1>Hi!</h1>
       <p className="lead">
-        I am a simple tool to count youtube channel subscriber with odometer and
-        chart that updates every second in real-time.
+        I am a simple tool that count youtube channel's subscriber and display
+        it with odometer and chart that updates every second in real-time.
       </p>
+      <p className="lead">
+        For this very first version is youtube and I am planning to add more
+        social media in the future.
+      </p>
+      <h1>Thank you!</h1>
     </React.Fragment>
   );
 };
